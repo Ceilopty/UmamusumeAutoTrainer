f867a7b3549314b1c539d4a1c2fcdaf472265ee4
Ceilopty <xuyechi@163.com>
2024-03-27 19:29:35 +0800

    team stadium修复

--- module/umamusume/script/team_stadium_task/info.py
+++ module/umamusume/script/team_stadium_task/info.py
@@ -7,6 +7,7 @@ from ..time_sale.info import TITLE as TS_TITLE
 TITLE = {
     "选择道具": script_team_stadium_select_item,
     "确认": script_team_stadium_no_rp,
+    "正在统计中": lambda ctx: ctx.ctrl.click(520, 830, "确认"),
 }
 
 

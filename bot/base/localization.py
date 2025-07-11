b6fe60ca10d08d4f121123a89faad43e89e0539c
zjc_he <zjc_he@sjtu.edu.cn>
2025-07-11 16:15:29 +0800

    fix: minor fix
    cherry-pick 09832a9b9c06fab92c2896599208f648b7a1af61

--- bot/base/localization.py
+++ bot/base/localization.py
@@ -61,8 +61,16 @@ class LocalizationMap:
 
     support_card = '暂无'
 
+
 localization_map = {attr: value for attr, value in vars(LocalizationMap).items()
                     if not callable(value) and not attr.startswith('_')}
+
+
+localization_map.update({
+    'True': '是',
+    'False': '否'
+})
+
 
 def _localization_single(string):
     for name, value in localization_map.items():
